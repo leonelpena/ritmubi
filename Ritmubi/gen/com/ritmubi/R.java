8ad195381e0aaa4ce959e08254024d7a409fbce9
Leo Peña <k_rokokop@hotmail.com>
2012-02-09 18:06:45 -0300

    Interfaz para varios dias

--- Ritmubi/gen/com/ritmubi/R.java
+++ Ritmubi/gen/com/ritmubi/R.java
@@ -25,22 +25,36 @@ public final class R {
         public static final int linearLayout1=0x7f050000;
         public static final int linearLayout2=0x7f050005;
         public static final int linearLayout3=0x7f05000b;
+        public static final int linearLayout4=0x7f050013;
+        public static final int linearLayout5=0x7f050011;
         public static final int numeroDiasRadioButton=0x7f050002;
         public static final int relativeLayout1=0x7f05000e;
+        public static final int selectorFechaInicio=0x7f050014;
         public static final int selectorFechaNacimiento=0x7f05000d;
+        public static final int spinner1=0x7f050017;
+        public static final int tableRow1=0x7f050015;
+        public static final int tableRow2=0x7f050019;
         public static final int textView1=0x7f050001;
         public static final int textView2=0x7f050006;
+        public static final int textView3=0x7f050012;
+        public static final int textView4=0x7f050016;
+        public static final int textView5=0x7f050018;
         public static final int tipoVisualizacionRadioButton=0x7f050007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int un_dia_activity=0x7f030001;
+        public static final int un_dia=0x7f030001;
+        public static final int varios_dias=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int biorritmoVariosDias=0x7f04000e;
         public static final int calcular=0x7f04000c;
         public static final int cuandoNacio=0x7f04000b;
+        public static final int desde=0x7f040010;
         public static final int dia=0x7f040003;
+        public static final int dias=0x7f040011;
+        public static final int hasta=0x7f04000f;
         public static final int metodoDeCalculo=0x7f040002;
         public static final int siguiente=0x7f040009;
         public static final int soloGrafico=0x7f040007;
