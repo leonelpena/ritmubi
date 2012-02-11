6a05584a6510fb137333fde3469c00114b925ea3
Leo Pena <k_rokokop@hotmail.com>
2012-02-11 17:21:25 -0300

    Botones "volver" habilitados

--- Ritmubi/src/com/ritmubi/UnDiaActivity.java
+++ Ritmubi/src/com/ritmubi/UnDiaActivity.java
@@ -2,10 +2,15 @@ package com.ritmubi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class UnDiaActivity extends Activity {
 	//private short tipoPresentacion;
+	private Button 
+				botonVolver,
+				botonCalcular;
 
 	/** Called when the activity is first created. */
     @Override
@@ -16,5 +21,22 @@ public class UnDiaActivity extends Activity {
         Bundle bundle = getIntent().getExtras();
         Toast.makeText(getApplicationContext(), "Un Dia, Presentacion: "+
         		bundle.getString("tipoPresentacion"), Toast.LENGTH_LONG).show();
+        
+        botonVolver = (Button) findViewById(R.id.botonVolver);
+        botonCalcular = (Button) findViewById(R.id.botonCalcular);
+        
+        botonVolver.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Cerramos la activity
+				finish();				
+			}
+		});
+        
+        botonCalcular.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "A calcular!!!",
+						Toast.LENGTH_LONG).show();				
+			}
+		});
     }
 }
--- Ritmubi/src/com/ritmubi/VariosDiasActivity.java
+++ Ritmubi/src/com/ritmubi/VariosDiasActivity.java
@@ -2,9 +2,15 @@ package com.ritmubi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class VariosDiasActivity extends Activity {
+	
+	private Button 
+				botonVolver,
+				botonCalcular;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,22 @@ public class VariosDiasActivity extends Activity {
         Bundle bundle = getIntent().getExtras();
         Toast.makeText(getApplicationContext(), "Varios Dias, Presentacion: "+
         		bundle.getString("tipoPresentacion"), Toast.LENGTH_LONG).show();
+    
+        botonVolver = (Button) findViewById(R.id.botonVolver);
+        botonCalcular = (Button) findViewById(R.id.botonCalcular);
+        
+        botonVolver.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Cerramos la activity
+				finish();				
+			}
+		});
+        
+        botonCalcular.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "A calcular!!!",
+						Toast.LENGTH_LONG).show();				
+			}
+		});
     }
 }
